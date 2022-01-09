7f96fbc1cf8c2016bd25e6d4e6a2f8b302c5f338
Chandula Janith <91379432+RedEdge967@users.noreply.github.com>
2022-01-09 15:19:53 +0530

    Update SiteConfig.ts

--- config/SiteConfig.ts
+++ config/SiteConfig.ts
@@ -2,20 +2,18 @@ export default {
   pathPrefix: '/', // Prefix for all links. If you deploy your site to example.com/portfolio your pathPrefix should be "portfolio"
 
   siteTitle: 'Typescript Power Blog', // Navigation and Site Title
-  siteTitleAlt: 'Typescript Power Blog - Gatsby Starter', // Alternative Site title for SEO
-  siteUrl: 'https://typescript-power-blog.github.com', // Domain of your site. No trailing slash!
+  siteTitleAlt: 'Typescript Power Blog - Chandula Janith', // Alternative Site title for SEO
+  siteUrl: 'https://gatsby-powerblog.vercel.app', // Domain of your site. No trailing slash!
   siteLanguage: 'en', // Language Tag on <html> element
   siteBanner: '/assets/banner.jpg', // Your image for og:image tag. You can find it in the /static folder
   defaultBg: '/assets/bg.png', // default post background header
   favicon: 'src/favicon.png', // Your image for favicons. You can find it in the /src folder
-  siteDescription: 'Typescript Power Blog with big typography', // Your site description
-  author: 'Majid Hajian', // Author for schemaORGJSONLD
+  siteDescription: 'Typescript Power Blog with big typography made by Chandula Janith (RedEdge967)', // Your site description
+  author: 'Chandula Janith', // Author for schemaORGJSONLD
   siteLogo: '/assets/logo.png', // Image for schemaORGJSONLD
 
-  // siteFBAppID: '123456789', // Facebook App ID - Optional
-  userTwitter: '@mhadaily', // Twitter Username - Optional
-  ogSiteName: 'mhadaily', // Facebook Site Name - Optional
-  ogLanguage: 'en_US', // Facebook Language
+  userGithub: 'RedEdge967',
+  ogLanguage: 'en_US',
 
   // Manifest and Progress color
   // See: https://developers.google.com/web/fundamentals/web-app-manifest/
