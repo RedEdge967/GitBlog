c8593b6905680e4a87eadbe8f5ff110d9b7dc64b
Chandula Janith <91379432+RedEdge967@users.noreply.github.com>
2022-01-09 15:40:19 +0530

    Update contact.tsx

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -16,7 +16,7 @@ export default () => {
       <Wrapper>
         <Content>
           <p>Super cool intro text to get people contacting me!</p>
-          <a href="https://www.twitter.com/mhadaily">
+          <a href="https://github.com/RedEdge967">
             <Button big={true}>
               <svg
                 width="1792"
@@ -26,10 +26,10 @@ export default () => {
               >
                 <path d="M1764 11q33 24 27 64l-256 1536q-5 29-32 45-14 8-31 8-11 0-24-5l-453-185-242 295q-18 23-49 23-13 0-22-4-19-7-30.5-23.5t-11.5-36.5v-349l864-1059-1069 925-395-162q-37-14-40-55-2-40 32-59l1664-960q15-9 32-9 20 0 36 11z" />
               </svg>
-              Twitter
+              Github
             </Button>
           </a>
-          <a href="https://www.linkedin.com/in/mhadaily">
+          <a href="https://lk.linkedin.com/in/chandula-janith-5529b7223">
             <Button big={true}>
               <svg
                 width="1792"
@@ -42,7 +42,7 @@ export default () => {
               Linkedin
             </Button>
           </a>
-          <a href="https://www.majidhajian.com">
+          <a href="https://rededge967.github.io">
             <Button big={true}>
               <svg
                 width="1792"
